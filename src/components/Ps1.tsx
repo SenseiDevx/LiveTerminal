8c57a79599a6b8ea9f902b57d324260832b768ff
sensei404 <kurmanbekovomurbek80@gmail.com>
2023-11-04 15:38:59 +0600

    fix bugs

--- src/components/Ps1.tsx
+++ src/components/Ps1.tsx
@@ -1,18 +1,18 @@
 import config from '../../config.json';
 
 export const Ps1 = () => {
-  return (
-    <div>
+    return (
+        <div>
       <span className="text-light-yellow dark:text-dark-yellow">
         {config.ps1_username}
       </span>
-      <span className="text-light-gray dark:text-dark-gray">@</span>
-      <span className="text-light-green dark:text-dark-green">
+            <span className="text-light-gray dark:text-dark-gray">@</span>
+            <span className="text-light-green dark:text-dark-green">
         {config.ps1_hostname}
       </span>
-      <span className="text-light-gray dark:text-dark-gray">:$ ~ </span>
-    </div>
-  );
+            <span className="text-light-gray dark:text-dark-gray">:$ ~ </span>
+        </div>
+    );
 };
 
 export default Ps1;
